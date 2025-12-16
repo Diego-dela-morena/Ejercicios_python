90cb5066f16118c6f4c8a2f1f71b09511b72f846
Diego-dela-morena <diegodelamorenamartinez@gmail.com>
2025-12-16 01:31:37 +0100

    Update Calculadora_angulo_critico.py

--- Calculadora_angulo_critico.py
+++ Calculadora_angulo_critico.py
@@ -4,7 +4,7 @@ while n2 > n1 or n1 <= 0:
     n1 = float(input("Ingrese el índice de refracción del primer medio n1: "))
     n2 = float(input("Ingrese el índice de refracción del segundo medio n2: "))
     if n2 > n1:
-        print("El primer índice de refracción (n1) debe ser mayor que el primero (n2) para que exista ángulo crítico.")  
+        print("El primer índice de refracción (n1) debe ser mayor que el segundo (n2) para que exista ángulo crítico.")  
     elif n1 <= 0:
         print("Los índices de refracción deben ser mayores que 0.")
 theta = math.degrees(math.asin(n2/n1))
