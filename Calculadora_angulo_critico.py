368eb8d20264b75e031ebc5799b9d08e4fb443b7
Diego-dela-morena <diegodelamorenamartinez@gmail.com>
2026-01-03 01:14:27 +0100

    Add scripts for calculating critical angles and refracted angles, and implement a daily expense tracker

--- Calculadora_angulo_critico.py
+++ Calculadora_angulo_critico.py
@@ -1,11 +1,11 @@
 import math
-n1,n2 = 1,0
-while n2 < n1 or n1 <= 0:
-    n1 = float(input("Ingrese el índice de refracción del primer medio n1 (si se encuentra en el aire n1 = 1): "))
+n1,n2 = 0,1
+while n2 > n1 or n1 <= 0:
+    n1 = float(input("Ingrese el índice de refracción del primer medio n1: "))
     n2 = float(input("Ingrese el índice de refracción del segundo medio n2: "))
-    if n2 < n1:
-        print("El segunde índice de refracción (n2) debe ser mayor que el primero (n1).")  
+    if n2 > n1:
+        print("El primer índice de refracción (n1) debe ser mayor que el segundo (n2) para que exista ángulo crítico.")  
     elif n1 <= 0:
         print("Los índices de refracción deben ser mayores que 0.")
-theta = math.degrees(math.asin(n1/n2))
+theta = math.degrees(math.asin(n2/n1))
 print(f"El ángulo crítico para la reflexión total es: {round(theta,4)}°.")
